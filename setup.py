d9d1e7985a3ee7019f446ab8ebf185995ca83a03
Rob Story <wrobstory@gmail.com>
2013-05-06 22:36:49 -0700

    Push to PyPI

--- setup.py
+++ setup.py
@@ -5,35 +5,21 @@ try:
 except ImportError:
     from distutils.core import setup
 
-
-classifiers = (
-    'Development Status :: 4 - Beta',
-    'Programming Language :: Python :: 2.7',
-    'License :: OSI Approved :: MIT License',
+setup(
+    name='bearcart',
+    version='0.1.0',
+    description='Python Pandas + Rickshaw.js = BearCart',
+    author='Rob Story',
+    author_email='wrobstory@gmail.com',
+    license='MIT License',
+    url='https://github.com/wrobstory/bearcart',
+    keywords='data visualization',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Programming Language :: Python :: 2.7',
+                 'License :: OSI Approved :: MIT License'],
+    packages=['bearcart'],
+    package_data={'': ['*.js',
+                       '*.css',
+                       'templates/*.html',
+                       'templates/*.js']}
 )
-
-required = (
-    'pandas'
-)
-
-kw = {
-    'name': 'bearcart',
-    'version': '0.1.0',
-    'description': 'Python Pandas + Rickshaw.js = BearCart',
-    'long_description': open('README.md', 'rt').read(),
-    'author': 'Rob Story',
-    'author_email': 'wrobstory@gmail.com',
-    'license': 'MIT License',
-    'url': 'https://github.com/wrobstory/bearcart',
-    'keywords': 'data visualization',
-    'classifiers': classifiers,
-    'packages': ['bearcart'],
-    'package_data': {'bearcart': ['bearcart/*.js',
-                                  'bearcart/*.css',
-                                  'bearcart/templates/*.html',
-                                  'bearcart/templates/*.js']},
-    'install_requires': required,
-    'zip_safe': True,
-}
-
-setup(**kw)
