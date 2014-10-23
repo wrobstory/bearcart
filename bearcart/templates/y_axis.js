ddfaff1fc0bb7f586022a23c9485e73062eeeb1b
Brendan Boerner <bboerner.foss@gmail.com>
2014-10-22 20:34:30 -0500

    Specify a width for y_axis to prevent large zone ofo white space to the left of the chart i.e. with this change chart is left justified on the page instead of quasi-centered.

--- bearcart/templates/y_axis.js
+++ bearcart/templates/y_axis.js
@@ -1,6 +1,7 @@
 var y_axis = new Rickshaw.Graph.Axis.Y( {
         graph: graph,
         orientation: 'left',
+        width: 30,
         height: {{ height }},
         tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
         element: d3.select("#{{ y_axis_id }}").node()
